2db6bf5995d3875bfae280973e86a7828662a3ff
Andrés Ortega <andythe20xd@gmail.com>
2025-10-16 22:48:17 -0300

    Light and dark color palette apllied to the app

--- app/src/main/java/com/example/appshop/ui/theme/Color.kt
+++ app/src/main/java/com/example/appshop/ui/theme/Color.kt
@@ -9,3 +9,19 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// --- Colores para la paleta clara personalizada ---
+val CreamPastel = Color(0xFFFFF5E1) // Fondo Principal
+val SoftPink = Color(0xFFFFC0CB) // Botones y elementos
+val Chocolate = Color(0xFF8B4513) // Botones y elementos
+val OffWhite = Color(0xFFF8F6F4) // Fondo de tarjetas y elementos secundarios
+val PastelPink = Color(0xFFFDEEEB) // Fondo de tarjetas y elementos secundarios
+val TextPrimary = Color(0xFF5D4037) // Marrón Oscuro
+val TextSecondary = Color(0xFFB0BEC5) // Gris Claro
+
+// --- Colores para la Paleta Oscura Personalizada ---
+
+val DarkChocolate = Color(0xFF211A18)
+val DarkSurface = Color(0xFF302826)
+val MutedPink = Color(0xFFE8A0B1)
+val LightBrown = Color(0xFFB89F83)
