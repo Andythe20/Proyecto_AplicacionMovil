36fedd2c533f6288ddce0e5050c0842e58fc971f
Jarod Smith <lbriones.dev@gmail.com>
2025-11-19 12:53:53 -0300

    Feat: Implementación de SwapeRefresh en ProductListScreen

--- app/src/main/java/com/example/appshop/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/appshop/viewmodel/ProductViewModel.kt
@@ -13,47 +13,20 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class ProductViewModel : ViewModel() {
-
-//    // Lista temporal de productos (en memoria)
-//    private val _productos = MutableStateFlow<List<Product>>(emptyList())
-//    val productos: StateFlow<List<Product>> = _productos
-//
-//    // Función para "guardar" producto (por ahora sólo en memoria)
-//    fun saveProduct(nombre: String, descripcion: String, precio: Double, codigo: String, fotoUri: Uri?) {
-//        val nuevo = Product(
-//            nombre = nombre,
-//            descripcion = descripcion,
-//            precio = precio,
-//            codigo = codigo,
-//            fotoUri = fotoUri
-//        )
-//
-//        _productos.value = _productos.value + nuevo
-//
-//        // TODO: Aquí agregar lógica para guardar en base de datos (Room o Firebase)
-//        // Ejemplo (más adelante):
-//        // viewModelScope.launch {
-//        //     productoRepository.insertarProducto(nuevo)
-//        // }
-//    }
-
-
     // CREA UNA INSTANCIA DEL REPOSITORIO
     // El repositorio necesita el servicio de la API, que lo obtenemos de RetrofitInstance.
     private val repository: ProductRepository
 
-    // ESTADO PARA LA LISTA DE PRODUCTOS
+    // ESTADOS PARA LOS PRODUCTOS Y EL ESTADO DE CARGA
     private val _products = MutableStateFlow<List<Product>>(emptyList())
-    val products: StateFlow<List<Product>> = _products.asStateFlow()
-
-    // ESTADO PARA SABER SI ESTÁ CARGANDO
-    // Útil para mostrar un círculo de progreso en la UI.
     private val _isLoading = MutableStateFlow(false)
+    private val _isRefreshing = MutableStateFlow(false)
+    val products: StateFlow<List<Product>> = _products.asStateFlow()
     val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+    val isRefreshing: StateFlow<Boolean> = _isRefreshing.asStateFlow()
 
     init {
         Log.d("ProductViewModel", "INIT DEL VIEWMODEL EJECUTADO")
-        Log.d("ProductViewModel", "URL base: http://107.22.181.238/api/v1/")
 
         // Inicializamos el repositorio aquí
         val apiService: IApiService = RetroFitInstance.api
@@ -74,8 +47,8 @@ class ProductViewModel : ViewModel() {
                 Log.d("ProductViewModel", "Productos desde API: $productList")
                 Log.d("ProductViewModel", "Cantidad: ${productList?.size}")
 
-                Log.d("ProductViewModel", "Respuesta recibida: ${productList != null}")
-                Log.d("ProductViewModel", "Cantidad de productos: ${productList?.size ?: 0}")
+                //Log.d("ProductViewModel", "Respuesta recibida: ${productList != null}")
+                //Log.d("ProductViewModel", "Cantidad de productos: ${productList?.size ?: 0}")
                 if (productList != null) {
                     // Actualiza la lista con los datos de la API
                     _products.value = productList
@@ -94,4 +67,24 @@ class ProductViewModel : ViewModel() {
         }
     }
 
+    // FUNCIÓN PARA PARA PULL-TO-REFRESH
+    fun refreshProducts() {
+        viewModelScope.launch {
+            _isRefreshing.value = true
+            try {
+                val productList = repository.getProducts()
+                Log.d("ProductViewModel", "Refresh - Productos desde API: ${productList?.size}")
+                if (productList != null) {
+                    _products.value = productList
+                } else {
+                    Log.e("ProductViewModel", "Refresh - La respuesta de la API fue nula")
+                    _products.value = emptyList()
+                }
+            } catch (e: Exception) {
+                Log.e("ProductViewModel", "Error al refrescar productos: ${e.message}")
+            } finally {
+                _isRefreshing.value = false
+            }
+        }
+    }
 }
