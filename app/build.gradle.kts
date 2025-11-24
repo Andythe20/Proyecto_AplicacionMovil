d860966df42fbf0508c2063fa16ea4f31265ea0a
L-Maulen <l.maulen@duocuc.cl>
2025-11-24 00:26:38 -0300

    Add testeos ViewModel Carrito

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.androidx.compose.material3.window.size.class1)
     implementation(libs.androidx.runtime)
     implementation(libs.androidx.compose.animation.core)
+    testImplementation(libs.junit.jupiter)
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
@@ -122,25 +123,9 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.2")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.2")
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+}
 
-
-    // Obligatorio para usar JUnit 5
-    tasks.withType<Test>().configureEach {
-        useJUnitPlatform()
-    }
+// Obligatorio para usar JUnit 5
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
